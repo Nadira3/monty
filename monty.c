f2c59b72d97d3ff1f011925513e507e049101c06
Nadira3 <113689650+Nadira3@users.noreply.github.com>
2023-06-21 22:25:00 -0700

    Revert "new uodate to test"

--- monty.c
+++ monty.c
@@ -7,29 +7,25 @@
  */
 int main(int ac, char **av)
 {
-	FILE *file = fopen(av[1], "r");
 	ssize_t input_len = 0;
-	size_t i = 0, line_number = 1;
-	global_t var
-	{
-		char *input = NULL;
-		char **input_token = NULL;
-		stack_t *head = NULL;
-	};
+	char *input = NULL, **input_token = NULL;
+	FILE *file = fopen(av[1], "r");
+	int i = 0, line_number = 1;
 
 	usage_check(ac, file, av[1]);
 
 	while (input_len != -1)
 	{
-		var.input = read_input(file, &input_len);
+		input = read_input(file, &input_len);
 		if (input_len == -1)
 			break;
-		var.input_token = parse_input();
-		execute(line_number);
-		free(var.input);
-		free_buf(var.input_token);
+		printf("%ld\n", input_len);
+		input_token = parse_input(input);
+		/* execute(input_token); */
+		free(input);
+		free_buf(input_token);
 	}
-	free(var.input);
+	free(input);
 	fclose(file);
 	return (0);
 }
