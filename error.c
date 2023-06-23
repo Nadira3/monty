6460a409075fb54096086b7c59de992709b0e5d9
Nadira3 <paitanun35@gmail.com>
2023-06-22 22:36:14 -0700

    print string function correction

--- error.c
+++ error.c
@@ -39,7 +39,7 @@ void print_string(va_list pam)
 {
 	char *ptr = va_arg(pam, char *);
 
-	fprintf(stderr, "%s", ptr ? ptr : "(nil)");
+	fprintf(stderr, "%s\n", ptr ? ptr : "(nil)");
 }
 
 /**
@@ -80,7 +80,6 @@ void err_and_exit(char *s, char **tok, FILE *f, const char * const format, ...)
 		i++;
 		va_end(pam);
 	}
-	puts("\n");
 	if (s)
 		free(s);
 	if (tok)
