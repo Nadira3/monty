26c65e1807d5b9fe613bd3c385abbad5ad5ee343
Nadira3 <paitanun35@gmail.com>
2023-06-22 22:01:54 -0700

    correction

--- instruction_four.c
+++ instruction_four.c
@@ -7,11 +7,20 @@
  */
 void rotr_func(stack_t **stack, unsigned int line_number)
 {
+	stack_t *temp, *ptr = *stack;
 	(void)line_number;
 
 	if (!isEmpty())
 	{
 		while ((*stack)->next)
-			*stack = (*stack)->next;	
+			*stack = (*stack)->next;
+		ptr = *stack;
+		while (ptr)
+		{
+			temp = ptr->next;
+			ptr->next = ptr->prev;
+			ptr->prev = temp;
+			ptr = ptr->next;
+		}
 	}
 }
